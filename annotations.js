c84130f5223a655d9d0bc61e7ed678427d7af19e
jaredscheib <jared.scheib@gmail.com>
2016-01-06 17:22:10 -0800

    (feat) Post annotations to Firebase

--- annotations.js
+++ annotations.js
@@ -1,3 +1,21 @@
-window.onload = function() {
-  document.getElementById('annotext').focus();
+var db = new Firebase('https://dazzling-heat-3394.firebaseio.com/');
+
+window.onload = function () {
+  var annotext = document.getElementById('annotext');
+  annotext.focus();
+
+  var submit = document.getElementById('submit');
+  submit.addEventListener('click', function (event) {
+    event.preventDefault();
+
+    var annotations = annotext.value;
+    annotext.value = '';
+    db.push(annotations);
+  });
 };
+
+db.on('child_added', function (snapshot){
+  var addedAnnotation = snapshot.val();
+  console.log(snapshot);
+  console.log(addedAnnotation);
+});
