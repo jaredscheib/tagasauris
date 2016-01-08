916811830502a90804ec8b3a1563e171c4b86c9e
jaredscheib <jared.scheib@gmail.com>
2016-01-07 16:42:11 -0800

    (fix) Move mturk helper functions to HIT page rather than server

--- annotations.js
+++ annotations.js
@@ -22,3 +22,21 @@ db.on('child_added', function (snapshot){
   console.log('Posted to Firebase:', addedAnnotation);
   mturkSubmit();
 });
+
+function mturkSubmit() {
+    var params = _.getUrlParams()
+    var f = $('<form action="' + params.turkSubmitTo + '/mturk/externalSubmit" method="GET"><input type="hidden" name="assignmentId" value="' + params.assignmentId + '"></input><input type="hidden" name="unused" value="unused"></input></form>')
+    $('body').append(f)
+    f.submit()
+};
+
+function mturkCheckPreview() {
+    var params = _.getUrlParams()
+    if (params.assignmentId == "ASSIGNMENT_ID_NOT_AVAILABLE") {
+        _.dialog($('<div style="background-color: rgba(0,0,0,0.5);color:white;font-size:xx-large;padding:10px"/>').text('preview'), false)
+        $('body').click(function () {
+            alert('This is a preview. Please accept the HIT to work on it.')
+        })
+        return true
+    }
+};
