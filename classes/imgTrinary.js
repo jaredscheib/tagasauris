774ec20a224263a91ad3d2480daf0eed7e550002
jaredscheib <jared.scheib@gmail.com>
2016-02-09 17:20:51 -0800

    Refactor as instantiator to return object, add prelim event handler

--- classes/imgTrinary.js
+++ classes/imgTrinary.js
@@ -1,71 +1,79 @@
-var makeImgTrinary = function (imgData, reqTicket, i) {
-  var trinaryContainer = document.createElement('div');
-  var pTrinary = document.createElement('p');
-  var imgTrinary = document.createElement('img');
-  var divTrinaryRadio = document.createElement('div');
-  var radioTrinary1 = document.createElement('input');
-  var labelTrinary1 = document.createElement('label');
-  var radioTrinary2 = document.createElement('input');
-  var labelTrinary2 = document.createElement('label');
-  var radioTrinary3 = document.createElement('input');
-  var labelTrinary3 = document.createElement('label');
+var ImgTrinary = function (imgData, reqTicket, i) {
+  this.trinaryContainer = document.createElement('div');
+  this.pTrinary = document.createElement('p');
+  this.imgTrinary = document.createElement('img');
+  this.divTrinaryRadio = document.createElement('div');
+  this.radioTrinary1 = document.createElement('input');
+  this.labelTrinary1 = document.createElement('label');
+  this.radioTrinary2 = document.createElement('input');
+  this.labelTrinary2 = document.createElement('label');
+  this.radioTrinary3 = document.createElement('input');
+  this.labelTrinary3 = document.createElement('label');
 
   var iTrinary = 'trinary' + String(i);
   var iTrinary_neg = iTrinary + '_-1';
   var iTrinary_mid = iTrinary + '_+0';
   var iTrinary_pos = iTrinary + '_+1';
 
-  trinaryContainer.id = iTrinary;
-  trinaryContainer.className = 'trinary_container';
+  this.trinaryContainer.id = iTrinary;
+  this.trinaryContainer.className = 'trinary_container';
 
-  pTrinary.className = 'trinary_p';
-  pTrinary.id = iTrinary + '_p';
-  pTrinary.innerHTML = imgData.query.split('_').join(' ');
+  this.pTrinary.className = 'trinary_p';
+  this.pTrinary.id = iTrinary + '_p';
+  this.pTrinary.innerHTML = imgData.query.split('_').join(' ');
 
-  imgTrinary.className = 'trinary_img';
-  imgTrinary.src = imgData.url;
+  this.imgTrinary.className = 'trinary_img';
+  this.imgTrinary.src = imgData.url;
 
-  divTrinaryRadio.className = 'div_trinary_radio';
+  this.divTrinaryRadio.className = 'div_trinary_radio';
 
-  radioTrinary1.setAttribute('type', 'radio');
-  radioTrinary1.setAttribute('name', iTrinary);
-  radioTrinary1.id = iTrinary_neg;
-  radioTrinary1.className = 'trinary_radio';
+  this.radioTrinary1.setAttribute('type', 'radio');
+  this.radioTrinary1.setAttribute('name', iTrinary);
+  this.radioTrinary1.setAttribute('value', -1);
+  this.radioTrinary1.id = iTrinary_neg;
+  this.radioTrinary1.className = 'trinary_radio';
 
-  labelTrinary1.setAttribute('for', iTrinary_neg);
-  labelTrinary1.className = 'trinary_label';
-  labelTrinary1.innerText = 'No Way';
+  this.labelTrinary1.setAttribute('for', iTrinary_neg);
+  this.labelTrinary1.className = 'trinary_label';
+  this.labelTrinary1.innerText = 'No Way';
 
-  radioTrinary2.setAttribute('type', 'radio');
-  radioTrinary2.setAttribute('name', iTrinary);
-  radioTrinary2.id = iTrinary_mid;
-  radioTrinary2.className = 'trinary_radio';
+  this.radioTrinary2.setAttribute('type', 'radio');
+  this.radioTrinary2.setAttribute('name', iTrinary);
+  this.radioTrinary2.setAttribute('value', 0);
+  this.radioTrinary2.id = iTrinary_mid;
+  this.radioTrinary2.className = 'trinary_radio';
 
-  labelTrinary2.setAttribute('for', iTrinary_mid);
-  labelTrinary2.className = 'trinary_label';
-  labelTrinary2.innerText = 'Unclear';
+  this.labelTrinary2.setAttribute('for', iTrinary_mid);
+  this.labelTrinary2.className = 'trinary_label';
+  this.labelTrinary2.innerText = 'Unclear';
 
-  radioTrinary3.setAttribute('type', 'radio');
-  radioTrinary3.setAttribute('name', iTrinary);
-  radioTrinary3.id = iTrinary_pos;
-  radioTrinary3.className = 'trinary_radio';
+  this.radioTrinary3.setAttribute('type', 'radio');
+  this.radioTrinary3.setAttribute('name', iTrinary);
+  this.radioTrinary3.setAttribute('value', 1);
+  this.radioTrinary3.id = iTrinary_pos;
+  this.radioTrinary3.className = 'trinary_radio';
 
-  labelTrinary3.setAttribute('for', iTrinary_pos);
-  labelTrinary3.className = 'trinary_label';
-  labelTrinary3.innerText = 'Definitely';
+  this.labelTrinary3.setAttribute('for', iTrinary_pos);
+  this.labelTrinary3.className = 'trinary_label';
+  this.labelTrinary3.innerText = 'Definitely';
 
-  trinaryContainer.appendChild(pTrinary);
-  trinaryContainer.appendChild(imgTrinary);
-  divTrinaryRadio.appendChild(radioTrinary1);
-  divTrinaryRadio.appendChild(labelTrinary1);
-  divTrinaryRadio.appendChild(radioTrinary2);
-  divTrinaryRadio.appendChild(labelTrinary2);
-  divTrinaryRadio.appendChild(radioTrinary3);
-  divTrinaryRadio.appendChild(labelTrinary3);
-  trinaryContainer.appendChild(divTrinaryRadio);
+  this.trinaryContainer.appendChild(this.pTrinary);
+  this.trinaryContainer.appendChild(this.imgTrinary);
+  this.divTrinaryRadio.appendChild(this.radioTrinary1);
+  this.divTrinaryRadio.appendChild(this.labelTrinary1);
+  this.divTrinaryRadio.appendChild(this.radioTrinary2);
+  this.divTrinaryRadio.appendChild(this.labelTrinary2);
+  this.divTrinaryRadio.appendChild(this.radioTrinary3);
+  this.divTrinaryRadio.appendChild(this.labelTrinary3);
+  this.trinaryContainer.appendChild(this.divTrinaryRadio);
 
-  trinaryContainer.imgData = imgData;
-  trinaryContainer.reqTicket = reqTicket;
+  this.imgData = imgData;
+  this.reqTicket = reqTicket;
+  this.resTicket = {};
 
-  return trinaryContainer;
+  this.addRadioChangeHandler = function (radio) {
+    radio.addEventListener('change', function (event) {
+      // if ()
+    }).call(this);;
+  };
 };
