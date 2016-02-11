2dae0f828e870bb64e9e60c9acaf409be9527fb9
jaredscheib <jared.scheib@gmail.com>
2016-02-10 20:39:50 -0800

    Merge loader into master

---
+++ classes/instructionsList.js
@@ -0,0 +1,13 @@
+var makeInstructionsList = function (arrInstructions, type) {
+  type = type || 'ol';
+  var instructionsList = document.createElement(type);
+  instructionsList.id = 'instructions_list';
+
+  arrInstructions.forEach(function (instruction) {
+    var instructionItem = document.createElement('li');
+    instructionItem.innerHTML = instruction;
+    instructionsList.appendChild(instructionItem);
+  });
+
+  return instructionsList;
+};
---
+++ tasks/task_img_annotation_annotator.js
@@ -0,0 +1,1 @@
+/* global jQuery, Firebase, _, anno */
