dfb371c9d63d11780e08b922035af62cf8bcf653
jaredscheib <jared.scheib@gmail.com>
2016-02-10 20:24:59 -0800

    Add checkbox for not_a_photo

--- classes/imgTrinary.js
+++ classes/imgTrinary.js
@@ -1,14 +1,21 @@
+/* global isTaskComplete */
+
 var ImgTrinary = function (imgData, reqTicket, i) {
+  this.i = i;
+
   this.trinaryContainer = document.createElement('div');
   this.pTrinary = document.createElement('p');
   this.imgTrinary = document.createElement('img');
   this.divTrinaryRadio = document.createElement('div');
+  this.divTrinaryCheckbox = document.createElement('div');
   this.radioTrinary1 = document.createElement('input');
   this.labelTrinary1 = document.createElement('label');
   this.radioTrinary2 = document.createElement('input');
   this.labelTrinary2 = document.createElement('label');
   this.radioTrinary3 = document.createElement('input');
   this.labelTrinary3 = document.createElement('label');
+  this.checkboxTrinary = document.createElement('input');
+  this.labelCheckboxTrinary = document.createElement('label');
 
   var iTrinary = 'trinary' + String(i);
   var iTrinary_neg = iTrinary + '_-1';
@@ -26,6 +33,7 @@ var ImgTrinary = function (imgData, reqTicket, i) {
   this.imgTrinary.src = imgData.url;
 
   this.divTrinaryRadio.className = 'div_trinary_radio';
+  this.divTrinaryCheckbox.className = 'div_trinary_checkbox';
 
   this.radioTrinary1.setAttribute('type', 'radio');
   this.radioTrinary1.setAttribute('name', iTrinary);
@@ -34,8 +42,9 @@ var ImgTrinary = function (imgData, reqTicket, i) {
   this.radioTrinary1.className = 'trinary_radio';
 
   this.labelTrinary1.setAttribute('for', iTrinary_neg);
-  this.labelTrinary1.className = 'trinary_label';
+  this.labelTrinary1.className = 'trinary_radio_label';
   this.labelTrinary1.innerText = 'No Way';
+  this.labelTrinary1.textContent = 'No Way';
 
   this.radioTrinary2.setAttribute('type', 'radio');
   this.radioTrinary2.setAttribute('name', iTrinary);
@@ -44,8 +53,9 @@ var ImgTrinary = function (imgData, reqTicket, i) {
   this.radioTrinary2.className = 'trinary_radio';
 
   this.labelTrinary2.setAttribute('for', iTrinary_mid);
-  this.labelTrinary2.className = 'trinary_label';
+  this.labelTrinary2.className = 'trinary_radio_label';
   this.labelTrinary2.innerText = 'Unclear';
+  this.labelTrinary2.textContent = 'Unclear';
 
   this.radioTrinary3.setAttribute('type', 'radio');
   this.radioTrinary3.setAttribute('name', iTrinary);
@@ -54,8 +64,20 @@ var ImgTrinary = function (imgData, reqTicket, i) {
   this.radioTrinary3.className = 'trinary_radio';
 
   this.labelTrinary3.setAttribute('for', iTrinary_pos);
-  this.labelTrinary3.className = 'trinary_label';
+  this.labelTrinary3.className = 'trinary_radio_label';
   this.labelTrinary3.innerText = 'Definitely';
+  this.labelTrinary3.textContent = 'Definitely';
+
+  this.checkboxTrinary.id = 'checkbox' + iTrinary
+  this.checkboxTrinary.className = 'trinary_checkbox';
+  this.checkboxTrinary.setAttribute('type', 'checkbox');
+  this.checkboxTrinary.setAttribute('name', 'checkIsRealPhoto' + iTrinary);
+  this.checkboxTrinary.setAttribute('value', 'isRealPhoto');
+  
+  this.labelCheckboxTrinary.setAttribute('for', 'checkIsRealPhoto' + iTrinary);
+  this.labelCheckboxTrinary.className = 'trinary_checkbox_label';
+  this.labelCheckboxTrinary.innerText = 'This image is not a real photo.';
+  this.labelCheckboxTrinary.textContent = 'This image is not a real photo.';
 
   this.trinaryContainer.appendChild(this.pTrinary);
   this.trinaryContainer.appendChild(this.imgTrinary);
@@ -65,7 +87,10 @@ var ImgTrinary = function (imgData, reqTicket, i) {
   this.divTrinaryRadio.appendChild(this.labelTrinary2);
   this.divTrinaryRadio.appendChild(this.radioTrinary3);
   this.divTrinaryRadio.appendChild(this.labelTrinary3);
+  this.divTrinaryCheckbox.appendChild(this.checkboxTrinary);
+  this.divTrinaryCheckbox.appendChild(this.labelCheckboxTrinary);
   this.trinaryContainer.appendChild(this.divTrinaryRadio);
+  this.trinaryContainer.appendChild(this.divTrinaryCheckbox);
 
   this.imgData = imgData;
   this.reqTicket = reqTicket;
@@ -73,8 +98,9 @@ var ImgTrinary = function (imgData, reqTicket, i) {
 
   this.addRadioChangeHandler = function (radio) {
     radio.addEventListener('change', function (event) {
-      console.log(event);
-      console.log(radio.value);
+      console.log('this', this);
+      console.log(event.target);
+      console.log(IsTaskComplete());
     });
   };
 
