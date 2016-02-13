e5043dedfba19219c988d3287c01d04bf4b87914
jaredscheib <jared.scheib@gmail.com>
2016-02-12 19:46:07 -0800

    Use loadscript promise, refactor db helper

--- public/tasks/task_img_verification_trinary.js
+++ public/tasks/task_img_verification_trinary.js
@@ -1,49 +1,64 @@
 /* global _, Promise, elements, getNow, minToMs, loadScript */
 /* global taskInfo, ticketsInfo, dbRef, taskData */
-/* global setImgCounters, removeLoader, makeInstructionsList, makeImgRow, ImgTrinary */
+/* global setImgCounters, makeInstructionsList, makeImgRow, ImgTrinary */
 
-var loadClasses = function (callback) {
-  loadScript('./classes/instructionsList.js', function () {
-    elements.instructionsArea.appendChild(makeInstructionsList([
-      'Does each of the <span class="img_counter"></span> photos below contain the named concept?'
-    ], 'li'));
-    loadScript('./classes/imgTrinary.js', function () {
-      loadScript('./classes/imgRow.js', function () {
-        callback();
-      });
-    });
-  });
-};
+console.log('a')
 
-loadClasses(function () {
-  stub_db.getTickets(taskInfo.taskName, taskInfo.ticketsToGet)
-    .then(function(reqTickets) {
-    // iterate over server response
-      // console.log('succeeded to get', reqTickets.length, 'of', taskInfo.ticketsToGet, 'tickets requested for task', taskInfo.taskName);
-      // console.log(reqTickets);
-      // instantiate imgTrinary class per image
-      removeLoader();
-      reqTickets.map(function(reqTicket, i){
-        if (i % 3 === 0) elements.mediaArea.appendChild(makeImgRow());
-        var newImgTrinary = new ImgTrinary(stub_db.getImage(reqTicket), reqTicket, i);
-        elements.resTicketItems.push(newImgTrinary);
-        elements.mediaArea.lastElementChild.appendChild(newImgTrinary.trinaryContainer);
-      // event listeners on trinary
-        // add response and other metadata (worker id, img_ref) to flat output obj based on trinary state change
-        // enable submitBtn if all images completed
-      });   
-      setImgCounters();
-    })
-    .catch(function(err) {
-      console.log('failed to get tickets for task');
-      throw err;
-    });
+loadClasses()
+.then(function() {
+  console.log('classes loaded');
+
+
     
   // event listener on submitBtn for click
     // iterate over results obj
-      // push to img_sanitizer_task_worker_res  
+      // push to img_sanitizer_task_worker_res
 });
+
+function loadClasses () {
+  var classPromises = [];
+
+  classPromises.push(
+    loadScript('./tasks/classes/instructionsList.js'),
+    loadScript('./tasks/classes/imgTrinary.js'),
+    loadScript('./tasks/classes/imgRow.js')
+  );
+
+  console.log('classPromises', classPromises);
+
+  return Promise.all(classPromises);
+};
 
 var isTaskComplete = function () {
   // return !$j('input[name=""')
 };
+
+function stub_updateComponentState(data) {
+ // TODO feed React
+  console.log('update compenents with data:', data)
+
+  elements.instructionsArea.appendChild(makeInstructionsList([
+    'Does each of the <span class="img_counter"></span> photos below contain the named concept?'
+  ], 'li'));
+
+  // instantiate imgTrinary class per image
+  // reqTickets.map(function(reqTicket, i){
+  //   if (i % 3 === 0) elements.mediaArea.appendChild(makeImgRow());
+  //   var newImgTrinary = new ImgTrinary(stub_db.getImage(reqTicket), reqTicket, i);
+  //   elements.resTicketItems.push(newImgTrinary);
+  //   elements.mediaArea.lastElementChild.appendChild(newImgTrinary.trinaryContainer);
+  // // event listeners on trinary
+  //   // add response and other metadata (worker id, img_ref) to flat output obj based on trinary state change
+  //   // enable submitBtn if all images completed
+  // });   
+  // setImgCounters();
+}
+
+function stub_getResultsData() {
+ // TODO get all results data in React style
+  return {
+    here: 'is',
+    is: 'an',
+    object: 'object'
+  };
+}
