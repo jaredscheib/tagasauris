144d736ae00c48806bf6ab14971a3982730bc2e9
jaredscheib <jared.scheib@gmail.com>
2016-02-12 17:34:15 -0800

    Clear old Firebase db helpers to prepare for message server

--- server/db.js
+++ server/db.js
@@ -2,184 +2,38 @@
 
 const _ = require('../scripts/helpers/utils.js');
 const Promise = require('bluebird');
+const fs = Promise.promisifyAll(require('fs'));
 const Firebase = require('firebase');
 
-var dbRef = {};
-dbRef.root = new Firebase('https://dazzling-heat-3394.firebaseio.com/');
-// .then(function (ref) {
-//   dbRef.imgTickets = ref;
-//   return dbRef.root.child(ticketsInfo.ticketsPool);
-// })
-// .then(function (ref) {
-//   dbRef.taskTicketsPool = ref;
-//   return dbRef.root.child(ticketsInfo.ticketsReq);
-// })
-// .then(function (ref) {
-//   dbRef.taskTicketsReq = ref;
-//   return dbRef.root.child(ticketsInfo.ticketsRes);
-// })
-// .then(function (ref) {
-//   dbRef.taskTicketsRes = ref;
-//   loadScript('tasks/' + taskInfo.taskName + '.js', function () {
-//     console.log('loaded task: ' + taskInfo.taskName);
-//   });
-// })
-// .catch(function (err) {
-//   console.log(err);
-// });
+var dbRef = new Firebase('https://dazzling-heat-3394.firebaseio.com/');
 
-// return dbRef.root.child('img_ref');
-
-var taskData = {};
-var taskInfo = {};
-var ticketsInfo = {};
-
-getOpenTickets('task_img_verification_trinary', 30, 15)
-.then(tickets => {
-  console.log('GOT TICKETS');
-  console.log(tickets);
-})
-.catch(err => {
-  console.log('err');
-  console.log(err);
-});
-
-// helper
-// if a ticket is in req and not expired, it's considered actively in process, so don't serve it
-function isInReqAndNotExpired (poolTicketToMatch) {
-  let fbQuery = dbRef.taskTicketsReq.orderByChild(ticketsInfo.ticketsPool_uid).equalTo(poolTicketToMatch.uid);
+function getOpenTickets (task, num) {
   return new Promise((resolve, reject) => {
-    fbQuery.once('value', matchedReqTickets => {
-      matchedReqTickets = matchedReqTickets.val();
-
-      for (var key in matchedReqTickets) {
-        let matchedReqTicket = matchedReqTickets[key];
-        if (isNotExpired(matchedReqTicket)) {
-          // console.log('in req and not expired!')
-          return resolve(true);
-        }
-      }
-      // console.log('not in req or expired!');
-      resolve(false);
+    console.log('getOpenTickets()');
+    stub_getOpenTickets(task, num)
+    .then(results => {
+      resolve(results);
     });
   });
 }
 
-// helper
-function isInRes (poolTicketToMatch) {
-  let fbQuery = dbRef.taskTicketsRes.orderByChild(ticketsInfo.ticketsPool_uid).equalTo(poolTicketToMatch.uid);
+// post tickets with collected results to database
+function closeTickets (tickets) {
   return new Promise((resolve, reject) => {
-    fbQuery.once('value', matchedResTickets => {
-      matchedResTickets = matchedResTickets.val();
-      console.log('matchedResTickets', matchedResTickets);
-      matchedResTickets ? resolve(true) : resolve(false);
-    });
+    console.log('closeTickets()');
+    stub_closeTickets(tickets)
+    .then(res => {
+      resolve(res);
+    })
   });
-}
-
-// helper
-function isNotExpired (reqTicket) {
-  return (_.getNow() - reqTicket.timeSubmitted) > reqTicket.taskDuration; // TODO create these keys, normalize taskDuration and reference it back from jobs bucket
-}
-
-// helper
-function isOpen (poolTicket) {
-  // console.log('isOpen?');
-  return Promise.all([isInRes(poolTicket), isInReqAndNotExpired(poolTicket)])
-  .then(resultsBoolArr => {
-    return resultsBoolArr.reduce((prevBool, currBool, i, o) => {
-      return prevBool && currBool;
-    }, true);
-  })
-}
-
-// API
-function getOpenTickets (taskName, maxCount, taskDuration) { // TODO can remove taskDuration from server, client, and db here once refactored probably
-  taskInfo.taskName = taskName;
-  taskInfo.maxTicketsToGet = Number(maxCount);
-  taskInfo.taskDuration = Number(taskDuration);
-
-  ticketsInfo.ticketsPool = taskInfo.taskName + '_tickets_pool',
-  ticketsInfo.ticketsReq = taskInfo.taskName + '_tickets_req',
-  ticketsInfo.ticketsRes = taskInfo.taskName + '_tickets_res',
-  ticketsInfo.ticketsPool_uid = ticketsInfo.ticketsPool + '_uid';
-  ticketsInfo.ticketsReq_uid = ticketsInfo.ticketsReq + '_uid';
-  
-  dbRef.imgRef = dbRef.root.child('img_ref');
-  dbRef.taskTicketsPool = dbRef.root.child(ticketsInfo.ticketsPool);
-  dbRef.taskTicketsReq = dbRef.root.child(ticketsInfo.ticketsReq);
-  dbRef.taskTicketsRes = dbRef.root.child(ticketsInfo.ticketsRes);
-
-  // console.log(taskInfo)
-  // console.log(ticketsInfo)
-
-  return new Promise(function(resolve, reject) {
-    // get unexpired tickets
-    
-    dbRef.root.child(ticketsInfo.ticketsPool)
-
-    dbRef.root.once('value', wholeDbSnapshot => {
-      taskData.all = wholeDbSnapshot.val();
-      taskData.imgTickets = taskData.all.img_ref;
-      taskData.taskTicketsPool = taskData.all[ticketsInfo.ticketsPool];
-      taskData.taskTicketsReq = taskData.all[ticketsInfo.ticketsReq] || {};
-      taskData.taskTicketsRes = taskData.all[ticketsInfo.ticketsRes] || {};
-
-      _.each(taskData, (item, key) => {
-        console.log(key, Object.keys(item).length);
-      });
-
-      resolve(taskData.taskTicketsPool)
-    }, err => { reject(err); });
-  })
-  .then(() => {
-    var ticketsToServe = [];
-    for (var poolTicketUID in taskData.taskTicketsPool) {
-      var poolTicket = taskData.taskTicketsPool[poolTicketUID];
-      if (ticketsToServe.length === maxCount) break;
-      isOpen (poolTicket)
-      .then(result => {
-        // console.log('found open ticket');
-        ticketsToServe.push(dbRef.taskTicketsReq.push(poolTicket)
-        .then(function (pushedReqTicketRef) {
-          // console.log('pushedReqTicketRef', pushedReqTicketRef);
-          this[ticketsInfo.ticketsPool_uid] = this.uid;
-          this.uid = pushedReqTicketRef.path.u[1];
-          this.timeSubmitted = _.getNow();
-          // this.taskDuration = _.minToMs(taskInfo.taskDuration); // TODO add this via reference to job entry?
-          delete this.img_ref_uid;
-
-          var pushObj = {};
-          pushObj[ticketsInfo.ticketsPool_uid] = this[ticketsInfo.ticketsPool_uid];
-          pushObj.uid = this.uid;
-          pushObj.timeSubmitted = this.timeSubmitted;
-          pushObj.taskDuration = this.taskDuration;
-          // console.log('pushObj', pushObj);
-          pushedReqTicketRef.set(pushObj);
-
-          return this;
-        }.bind(_.deepClone(poolTicket)))
-        .catch(function (err) {
-          throw err;
-        }));
-        
-      });
-    }
-    taskInfo.ticketsReceived = ticketsToServe.length;
-    console.log('ticketsToServe.length', ticketsToServe.length);
-    return Promise.all(ticketsToServe);
-  })
-  .catch(err => {
-    sendErr(err);
-  }); 
-}
-
-// helper
-function getImage (reqTicket) {
-  var ticketsPoolKey = reqTicket[ticketsInfo.ticketsPool_uid];
-  var ticketsPoolTask = taskData.taskTicketsPool[ticketsPoolKey];
-  var gottenImg = taskData.imgTickets[ticketsPoolTask.img_ref_uid];
-  return gottenImg;
+  // var postRef = dbRef.root.child(ticketsInfo.ticketsRes);
+  // elements.resTicketItems.forEach(function (resTicketItem) {
+  //   resTicketItem.resTicket[ticketsInfo.ticketsReq_uid] = resTicketItem.resTicket.reqTicket.uid;
+  //   resTicketItem.resTicket.workerId = params.workerId;
+  //   resTicketItem.resTicket.timeSubmitted = _.getNow();
+  //   postRef.push(resTicketItem.resTicket);
+  // });
+  // return;
 }
 
 // helper
@@ -193,7 +47,7 @@ function pushAndAddUID (targetRef, sourceObj) {
   .then(function () {
     // console.log('item at ' + targetRef.path.u[0] + '/' + item.uid + 'updated');
   })
-  .catch(function (err) { throw (err); });
+  .catch(sendErr);
 }
 
 function sendErr (err) {
@@ -201,6 +55,22 @@ function sendErr (err) {
   return err;
 }
 
+function stub_getOpenTickets (task, num) {
+  return fs.readFileAsync('./fixtures/porsche_macan.json', 'utf-8')
+  .then(data => {
+    var x = JSON.parse(data).slice(0, 30);
+    console.log(x.length);
+    return x;
+  });
+}
+
+function stub_closeTickets (tickets) {
+  return new Promise((resolve, reject) => {
+    resolve('yes');
+  });
+}
+
 module.exports = {
   getOpenTickets: getOpenTickets,
+  closeTickets: closeTickets
 }
