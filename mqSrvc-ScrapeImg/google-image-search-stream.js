1e81519fde77a57bbd8946c76ea6f9a03713fbdc
jaredscheib <jared.scheib@gmail.com>
2016-02-14 03:22:59 -0800

    Add Google image search img refs results

---
+++ mqSrvc-ScrapeImg/google-image-search-stream.js
@@ -0,0 +1,72 @@
+'use strict';
+
+const got = require('got');
+
+function GoogleClient (cse_id, api_key) {
+  if (!(this instanceof GoogleClient)) {
+    return new GoogleClient(cse_id, api_key);
+  }
+
+  this.endpoint = 'https://www.googleapis.com/customsearch/v1?';
+  this.cx = cse_id;
+  this.key = api_key;
+}
+
+GoogleClient.prototype._buildOptions = function (query, options) {
+  let build = {
+    key: this.key,
+    cx: this.cx,
+    q: query.replace(/\s/g, '+'),
+    searchType: 'image',
+    imgType: 'photo',
+    start: 1,
+    num: 10,
+    json: true
+  };
+
+  for (var key in options) {
+    build[key] = options[key];
+  }
+
+  return build;
+}
+
+GoogleClient.prototype._buildResponse = function (res) {
+  return res.body.items.map(function (item) {
+    return {
+      type: item.mime,
+      width: item.image.width,
+      height: item.image.height,
+      size: item.image.byteSize,
+      web_url: item.link,
+      thumbnail_url: item.image.thumbnailLink,
+      thumbnail_width: item.image.thumbnailWidth,
+      thumbnail_height: item.image.thumbnailHeight
+    };
+  });
+};
+
+GoogleClient.prototype.search = function (query, maxResults) {
+  if (!query) {
+    throw new TypeError('Expected a query');
+  }
+
+  if (maxResults < 1) throw 'Must search for at least 1 result';
+  console.log(query, maxResults);
+
+  let reqQueriesToResolve = [];
+  for (let start = 1; start <= maxResults; start += 10) {
+    let num = maxResults - start + 1;
+    reqQueriesToResolve.push(
+      got(this.endpoint + '/customsearch/v1', {
+        query: this._buildOptions(query, { start: start, num: num < 10 ? num : 10 }),
+        json: true
+      })
+      .then(this._buildResponse)
+    );
+  }
+  
+  return Promise.all(reqQueriesToResolve)
+};
+
+module.exports = GoogleClient;
--- mqSrvc-ScrapeImg/index.js
+++ mqSrvc-ScrapeImg/index.js
@@ -1,28 +1,42 @@
 'use strict';
 
 const MsgQueueClient = require('msgqueue-client');
+const GoogleClient = require('./google-image-search-stream.js');
 
 const mqServerConfig = require('../common/config/mqserver.js');
+const secrets = require('../common/secrets/scrape-img.js');
 
 const mq = new MsgQueueClient(`${mqServerConfig.url}:${mqServerConfig.port}`, { log: true });
+const gClient = new GoogleClient(secrets.CSE_ID, secrets.API_KEY);
 
 mq.on('connected', () => { console.log('connected to mq'); });
 
 mq.listen('image_scrape_req', (ack, reject, payload) => {
-  // TODO
-  // fire off query via search clients
-  // getImgRefs
-
-    // save images to S3
-
-
-    // save successfully scraped image refs to database
-
-    // then enqueue final results to mq
+    // fire off query via search clients
+  getWebImgRefs(query, num)
+  .then(results => {
+    console.log('results');
+    console.log(results);
     let queue = 'image_scrape_res';
-    mq.enqueue(queue, { dummy: 'dummy' }) // TODO add payload (firebase ref to obj that contains img ref to S3 image)
+    mq.enqueue(queue, results[0]) // TODO add payload (firebase ref to obj that contains img ref to S3 image)
     .then(() => {
       console.log('service: image_scrape_req --> image_scrape_res')
       ack();
     });
+  });
+  // .then(uploadToS3)
+    // require('aws-sdk');
+    // require('s3-upload-stream');
+    // set up config file for now, env variables later
+    // follow example at https://www.npmjs.com/package/s3-upload-stream
+  // .then(pushToDb)
+  // .then()
+
+    // save successfully scraped image refs to database
+
+    // then enqueue final results to mq
 });
+
+function getWebImgRefs (query, num) {
+  return gClient.search('porsche macan', 11)
+}
