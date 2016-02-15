646052174aef5f46dc9f9eeeaa45cbef9e663e52
jaredscheib <jared.scheib@gmail.com>
2016-02-14 22:36:04 -0800

    Implement pipe and transform and upload to s3 for set of images

--- mqSrvc-ScrapeImg/s3.js
+++ mqSrvc-ScrapeImg/s3.js
@@ -7,108 +7,115 @@ const url = require('url');
 const fs = require('fs');
 const sharp = require('sharp');
 const AWS = require('aws-sdk');
-const credentials = require('../common/secrets/aws.js').S3_credentials;
-// const s3 = new AWS.S3(credentials);
+const credentials = require('../common/secrets/aws.json');
+const s3_fixtures = require('../common/fixtures/s3.json');
+const s3 = new AWS.S3(credentials);
 // Promise.promisifyAll(Object.getPrototypeOf(s3));
-const s3Stream = require('s3-upload-stream')(new AWS.S3(credentials));
+const s3Stream = require('s3-upload-stream')(s3);
 
-let mySourceDirRef = './test_bucket';
-let myTargetDirRef = './test_bucket/output';
-let myBucketRef = 'vid_decomp';
+function pipeTransformAndUploadToS3 (bucket) {
+  return new Promise((resolve, reject) => {
+    let file = this.orig_url.split('/').reverse()[0];
+    let name = file.split('.');
+    let ext = name.pop();
+    ext = ext === 'jpg' ? 'jpeg' : ext;
 
-let myImgSet = [
-  'http://gtspirit.com/wp-content/uploads/2015/09/Mansory-Porsche-Macan-2.jpg',
-  'http://pngimg.com/upload/audi_PNG1736.png',
-  'https://i.ytimg.com/vi/0E05jQYpdpE/maxresdefault.jpg'
-];
+    let options = [
+      { width: 'full', quality: 100 },
+      { width: 300, quality: 40 },
+      { width: 600, quality: 80 },
+    ];
+    
+    // let writableStream = fs.createWriteStream(`${bucket}/${file}`);
+    let writableStreams = options.map((item, i) => {
+        return s3Stream.upload({
+          Bucket: bucket,
+          Key: `${name}-w${options[i].width}-q${options[i].quality}.${ext}`,
+          ACL: 'public-read',
+          StorageClass: 'REDUCED_REDUNDANCY',
+          ContentType: this.type
+        })
+        .on('error', err => {
+          console.log(err, err.stack);
+          reject(err);
+        })
+        .on('uploaded', details => {
+          this.s3_url = details.Location;
+          this.s3_bucket = details.Bucket;
+          this.s3_key = details.Key;
+          this.s3_etag = details.ETag;
+          console.log(`uploaded ${file} to ${this.s3_bucket} on s3`);
+          resolve(this);
+        });
+      });
 
-let myFileSet = [
-  'Mansory-Porsche-Macan-2.jpg',
-  'audi_PNG1736.png',
-  'maxresdefault.jpg'
-];
+    let pipeline = sharp().withMetadata().on('error', err => { console.log(err); });
+    pipeline.pipe(writableStreams[0])
+    pipeline.clone().resize(options[1].width).quality(options[1].quality).withMetadata().pipe(writableStreams[1]);
+    pipeline.clone().resize(options[2].width).quality(options[2].quality).withMetadata().pipe(writableStreams[2]);
 
-function pipeImgToBucket (url, bucket) {
-  let file = url.split('/').reverse()[0];
-  let name = file.split('.');
-  let ext = name.pop();
-  
-  // let writableStream = fs.createWriteStream(`${bucket}/${file}`);
-  let writableStreams = [];
-  writableStreams.push(s3Stream.upload({
-    Bucket: bucket,
-    Key: file
-  }));
-
-  let options = {
-    width: [400, 800],
-    quality: [50, 80]
-  };
-
-  let writableStream1 = fs.createWriteStream(writePath1);
-  let writableStream2 = fs.createWriteStream(writePath2);
-
-  let pipeline = sharp();
-  pipeline.pipe(writableStream0);
-  pipeline.clone().resize(options.width[0]).quality(options.quality[0]).pipe(writableStream1);
-  pipeline.clone().resize(options.width[1]).quality(options.quality[1]).pipe(writableStream2);
-  readableStream.pipe(pipeline);
-
-  request.get(url) // readableStream
-  .on('response', response => {
-    console.log(response.statusCode);
-    console.log(response.headers['content-type']);
-  })
-  .on('error', err => {
-    console.log('error:', err);
-  })
-  .pipe(writableStream);
-
-  writableStream.on('error', (err) => {
-    console.log(err);
-  })
-
-  writableStream.on('close', (res) => {
-    console.log('finished write stream', file, res);
-    // createResizedImg(file);
+    var req = request.get(this.orig_url) // readableStream
+    .on('response', response => {
+      console.log(response.statusCode);
+      console.log(response.headers['content-type']);
+      if (response.headers['content-type'].split('/')[0] === 'image' ) {
+        console.log('URL resolved to image');
+        req.pipe(pipeline)
+      } else {
+        resolve({}); // TODO come up with solution for when image url does not resolve to an image successfully
+      }
+    })
+    .on('error', err => {
+      console.log(err, err.stack);
+      reject(err);
+    });
+    // .pipe(pipeline);
   });
 }
 
 function pipeImgSetToBucket (imgSet, bucket) {
-  imgSet.forEach(url => {
-    pipeImgToBucket(url, bucket);
+  // let pipePromises = [];
+  imgSet.forEach(imgObj => {
+    pipeTransformAndUploadToS3.call(imgObj, bucket)
+    .then(obj => {
+      console.log('resolved pipePromise', obj);
+      // console.log('resolved pipePromise', obj.details);
+    });
   });
+  // return Promise.all(pipePromises);
 }
 
-// for each image
-  // create a writable stream
-  // multiplex
-  // pipe to multiplex
+pipeImgSetToBucket(s3_fixtures.myImgObjs, s3_fixtures.myBucketRef)
+// .then(res => {
+//   console.log('all promises resolved, images uploaded');
+// });
+// .then(res => {
+//   console.log('piped', res);
+  // s3Stream.listObjects({ Bucket: myBucketRef }, (err, res) => {
+  //   if (err) console.log(err, err.stack);
+  //   else console.log(res);
+  // });
+// });
 
+// _(myFileSet).map(file => {
+//   let options = {
+//     width: [400, 800],
+//     quality: [50, 80]
+//   };
 
+//   let readableStream = fs.createReadStream(`${mySourceDirRef}/${file}`);
+//   let pipeline = multiplexToLocalFiles(file, options, myTargetDirRef);
+//   return readableStream.pipe(pipeline);
+// })
+// .each(imgStream => {
+//   console.log('imgStream', typeof imgStream);
+//   imgStream.metadata()
+//   .then(metadata => {
+//     console.log('metadata', metadata);
+//   })
+// })
 
-
-// pipeImgSetToBucket(myImgSet, myBucketRef);
-
-_(myFileSet).map(file => {
-  let options = {
-    width: [400, 800],
-    quality: [50, 80]
-  };
-
-  let readableStream = fs.createReadStream(`${mySourceDirRef}/${file}`);
-  let pipeline = multiplex(file, options, myTargetDirRef);
-  return readableStream.pipe(pipeline);
-})
-.each(imgStream => {
-  console.log('imgStream', typeof imgStream);
-  imgStream.metadata()
-  .then(metadata => {
-    console.log('metadata', metadata);
-  })
-})
-
-function multiplex (file, options, targetDir) {
+function multiplexToLocalFiles (file, options, targetDir) {
   let name = file.split('.');
   let ext = name.pop();
 
@@ -130,21 +137,21 @@ function multiplex (file, options, targetDir) {
 
 // AWS helpers
 
-function createBucket () {
-  s3.createBucketAsync({Bucket: 'vid_decomp'})
-  .then((res) => {
-    console.log('res', res)
-    var params = {Bucket: 'vid_decomp', Key: 'test', Body: 'testStr'};
+// function createBucket () {
+//   s3.createBucketAsync({Bucket: 'vid_decomp'})
+//   .then((res) => {
+//     console.log('res', res)
+//     var params = {Bucket: 'vid_decomp', Key: 'test', Body: 'testStr'};
 
-    return s3.putObjectAsync(params);
-  })
-  .then(data => {
-    console.log('data', data);
-  })
-  .catch(throwErr);
-}
+//     return s3.putObjectAsync(params);
+//   })
+//   .then(data => {
+//     console.log('data', data);
+//   })
+//   .catch(throwErr);
+// }
 
-function throwErr (err) {
-  console.log(err);
-  throw err;
-}  
+// function throwErr (err) {
+//   console.log(err);
+//   throw err;
+// }  
