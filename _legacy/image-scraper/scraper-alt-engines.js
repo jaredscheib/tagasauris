fd54cff2d97f2d826b97877d14de2cf39c73d308
jaredscheib <jared.scheib@gmail.com>
2016-02-14 18:58:40 -0800

    Modify legacy code

--- _legacy/image-scraper/scraper-alt-engines.js
+++ _legacy/image-scraper/scraper-alt-engines.js
@@ -1,39 +1,35 @@
 'use strict';
 
 const Promise = require('bluebird');
-const _ = require('underscore');
-const fs = Promise.promisifyAll(require('fs'));
+// const _ = require('underscore');
+// const fs = Promise.promisifyAll(require('fs'));
 const Scraper = require('images-scraper');
 const google = new Scraper.Google();
 const bing = new Scraper.Bing();
-const yahoo = new Scraper.Yahoo();
-const picsearch = new Scraper.Picsearch();
+// const yahoo = new Scraper.Yahoo();
+// const picsearch = new Scraper.Picsearch();
 
-//photo only
-//google: &tbs=itp:photo
-//bing: &qft=+filterui:photo-photo
+// photo only
+// google: &tbs=itp:photo
+// bing: &qft=+filterui:photo-photo
 
-// google.list({
-//   keyword: 'banana',
-//   num: 10,
-//   detail: true,
-//   nightmare: {
-//     show: true
-//   }
-// })
-// .then(function (res) {
-//   console.log('first 10 results from google', res);
-// }).catch(function(err) {
-//   console.log('err', err);
-// });
- 
-// // you can also watch on events 
-// google.on('result', function (item) {
-//   console.log('out', item);
-// });
+function searchGoogle(queryString, maxResultsNum) {
+  return google.list({
+    keyword: queryString,
+    num: maxResultsNum,
+    detail: true,
+    nightmare: {
+      show: false
+    }
+  });
+  // // you can also watch on events
+  // google.on('result', function (item) {
+  //   console.log('out', item);
+  // });
+}
 
 function normalizeBingToGoogle(imgObjSet) {
-  return _.map(imgObjSet, (item) => {
+  return imgObjSet.map(item => {
     let imgObj = item;
     imgObj.thumbnail_url = imgObj.thumb;
     delete imgObj.thumb;
@@ -44,20 +40,25 @@ function normalizeBingToGoogle(imgObjSet) {
 }
 
 function searchBing(queryString, maxResultsNum) {
-  bing.list({
-    keyword: queryString,
-    num: maxResultsNum,
-    detail: true
-  })
-  .then(res => {
-    const normalRes = normalizeBingToGoogle(res);
-    writeFile(`./results/${queryString.replace(' ', '_')}.js`, normalRes);
-    writeFile(`./results/${queryString.replace(' ', '_')}.json`, normalRes);
-  }).catch(err => {
-    throw err;
+  return new Promise((resolve, reject) => {
+    bing.list({
+      keyword: queryString,
+      num: maxResultsNum,
+      detail: true
+    })
+    .then(res => {
+      resolve(normalizeBingToGoogle(res));
+      // writeFile(`./results/${queryString.replace(' ', '_')}.js`, normalRes);
+      // writeFile(`./results/${queryString.replace(' ', '_')}.json`, normalRes);
+    })
+    .catch(err => {
+      reject(err);
+    });
   });
 }
 
+
 module.exports = {
+  searchGoogle: searchGoogle,
   searchBing: searchBing,
 };
