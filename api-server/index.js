2d74acd9fe96346ed99ba8ac8ae2910968afeabc
jaredscheib <jared.scheib@gmail.com>
2016-02-14 18:58:13 -0800

    Have api-server communicate with mq controller

--- api-server/index.js
+++ api-server/index.js
@@ -13,9 +13,11 @@ mq.on('connected', () => { console.log('connected to mq'); });
 
 app.set('port', config.port);
 
+// curl 'http://localhost:65000/images?query=porsche+macan&num=50&task=img_verification'
 app.get('/images', (req, res) => {
   console.log('GET on /images');
-  mq.enqueue('image_scrape_req', req.body);
+  if (req.query.num > 1000) res.status(400).send('Can only query for up to 1000 images currently.');
+  mq.enqueue('ctrl_sns_img_scrape_req', req.query); // query, num, task (optional)
   res.status(200).send('images');
 });
 
--- mqCtrl-ScrapeAndServe/index.js
+++ mqCtrl-ScrapeAndServe/index.js
@@ -8,10 +8,18 @@ const mq = new MsgQueueClient(`${mqServerConfig.url}:${mqServerConfig.port}`, { 
 
 mq.on('connected', () => { console.log('connected to mq'); });
 
-mq.listen('image_scrape_res', (ack, reject, payload) => {
+mq.listen('ctrl_sns_img_scrape_req', (ack, reject, payload) => {
+  mq.enqueue('srvc_img_scrape_req', payload) // TODO determine which task this can serve
+  .then(() => {
+    console.log('controller: ctrl_sns_img_scrape_req --> srvc_img_scrape_req');
+    ack();
+  });
+});
+
+mq.listen('ctrl_img_scrape_res', (ack, reject, payload) => {
   mq.enqueue('ticket_gen_req', payload) // TODO determine which task this can serve
   .then(() => {
-    console.log('controller: image_scrape_res --> ticket_get_req')
+    console.log('controller: img_scrape_res --> ticket_get_req');
     ack();
   });
 });
