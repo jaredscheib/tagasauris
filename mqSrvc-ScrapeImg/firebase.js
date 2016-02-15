4cab2b0a3291a2eb5d76af32dc40c7c0e131bf08
jaredscheib <jared.scheib@gmail.com>
2016-02-15 06:01:29 -0800

    Hard-code firebase directory ref

--- mqSrvc-ScrapeImg/firebase.js
+++ mqSrvc-ScrapeImg/firebase.js
@@ -6,15 +6,18 @@ const firebase_secrets = require('../common/secrets/scrape-img.js').firebase;
 
 const dbRef = new Firebase(firebase_secrets.url);
 
-function pushAndAddUID(sourceObj, targetRef) {
+function pushAndAddUID(sourceObj) {
   console.log(`attempt to pushAndAddUID`)
-  targetRef = targetRef || dbRef.child('img_ref3');
+  // let targetBucket = sourceObj.s3_key.split('/').slice(0,2).join('/');
+  // console.log('targetBucket', targetBucket);
+  let targetRef = dbRef.child('img_ref') || dbRef.child('img_ref3');
   const item = sourceObj;
   return new Promise((resolve, reject) => {
     targetRef.push(item)
     .then(tempRef => {
       item.uid = tempRef.path.u[1];
       tempRef.update({ uid: item.uid });
+      // console.log('tempRef', tempRef);
       resolve(tempRef); // return updated obj ref
     })
     // .then(() => {
